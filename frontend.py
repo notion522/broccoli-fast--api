270deacf80b389cac7145b9480716de1a751fa90
K.hegde <khyatihegde@gmail.com>
2025-11-06 17:10:16 +0530

    rendered changes of render deployement commit

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # API_URL = "http://34.226.152.222:8000/predict" 
-API_URL = "https://broccoli-fast-api.onrender.com"
+API_URL = "https://broccoli-fast-api.onrender.com/predict"
 # API_URL = "http://127.0.0.1:8000/predict"
 
 st.title("Insurance Premium Category Predictor")
